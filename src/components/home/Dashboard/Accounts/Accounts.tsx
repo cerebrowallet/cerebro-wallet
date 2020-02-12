776a93075c1ab5fd71b9e787c40baf5173722f8d
Pavel Andrianov <mail@pavelandrianov.ru>
2020-02-12 11:57:05 +0530

    Add amount in local currency in dashboard accounts

--- src/components/home/Dashboard/Accounts/Accounts.tsx
+++ src/components/home/Dashboard/Accounts/Accounts.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Plus as PlusIcon } from 'react-feather';
 
-import { getAccountsList } from '../../../../store/account/selectors';
+import {
+  getAccountsList,
+  getExchangeRates,
+} from '../../../../store/account/selectors';
 import { Account } from '../../../../store/account/types';
+import { config } from '../../../../config';
+import { getSettings } from '../../../../store/user/selectors';
+import { CurrencySymbols } from '../../../../dictionaries';
 
 import {
   AccountsContainer,
@@ -16,10 +22,11 @@ import {
 
 import Scrollbar from '../../../shared/Scrollbar/Scrollbar';
 import CurrencyIcon from '../../../shared/CurrencyIcon/CurrencyIcon';
-import { config } from '../../../../config';
 
 const Accounts: React.FC = () => {
   const accounts: Account[] = useSelector(getAccountsList);
+  const rates = useSelector(getExchangeRates);
+  const settings = useSelector(getSettings);
 
   return (
     <AccountsContainer>
@@ -36,7 +43,13 @@ const Accounts: React.FC = () => {
             icon={<CurrencyIcon coin={account.coin} size="lg" />}
             link={`/account/${account.id}`}
             text={account.name}
-            descText={`${account.balance} ${config.coins[account.coin.toUpperCase()].abbr} / $100`}
+            descText={`${account.balance} ${
+              config.coins[account.coin].abbr
+            } / ${settings.currency && CurrencySymbols[settings.currency]}${
+              rates && settings.currency
+                ? account.balance * rates[account.coin][settings.currency]
+                : 0
+            }`}
           />
         ))}
       </Scrollbar>
