874ddf313b7e2db739925a99382a9797810f269d
Pavel Andrianov <mail@pavelandrianov.ru>
2020-05-14 07:15:35 +0800

    Replace fonts

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -1,13 +1,34 @@
 import { createGlobalStyle } from 'styled-components';
 
+import BasierCircleRegularWoff from './fonts/basiercircle-regular-webfont.woff';
+import BasierCircleRegularWoff2 from './fonts/basiercircle-regular-webfont.woff2';
+import BasierCircledSemiBoldWoff from './fonts/basiercircle-semibold-webfont.woff';
+import BasierCircleSemiBoldWoff2 from './fonts/basiercircle-semibold-webfont.woff2';
+
 import { Theme } from '../utils/styled';
 
 const Globals = createGlobalStyle<{ theme: Theme }>`
+  @font-face {
+    font-family: 'basier_circle';
+    src: url(${BasierCircleRegularWoff2}) format('woff2'),
+         url(${BasierCircleRegularWoff}) format('woff');
+    font-weight: normal;
+    font-style: normal;
+  }
+  
+  @font-face {
+    font-family: 'basier_circle';
+    src: url(${BasierCircleSemiBoldWoff2}) format('woff2'),
+         url(${BasierCircledSemiBoldWoff}) format('woff');
+    font-weight: 600;
+    font-style: normal;
+  }
+
   body {
     line-height: 1.5;
-    color: ${props => props.theme.colors.primary};
-    background: ${props => props.theme.colors.tertiary};
-    font-family: 'Montserrat', sans-serif;
+    color: ${(props) => props.theme.colors.primary};
+    background: ${(props) => props.theme.colors.tertiary};
+    font-family: 'basier_circle', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
@@ -21,7 +42,7 @@ const Globals = createGlobalStyle<{ theme: Theme }>`
   
   p a {
     text-decoration: none;
-    color: ${props => props.theme.colors.secondary};
+    color: ${(props) => props.theme.colors.secondary};
     transition: color .15s ease-in;
     
     &:hover {
