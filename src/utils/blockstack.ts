3c08502545b6367a10cdfd14a3369391b43c505e
Pavel Andrianov <mail@pavelandrianov.ru>
2020-05-24 07:17:45 +0800

    Propagate Gaia wrapper errors

--- src/utils/blockstack.ts
+++ src/utils/blockstack.ts
@@ -5,18 +5,13 @@ const appConfig = new AppConfig(undefined, undefined, '/auth-callback');
 export const userSession = new UserSession({ appConfig });
 
 export const getFile = async (fileName: string) => {
-  try {
-    const response = await userSession.getFile(fileName, { decrypt: true });
+  const response = await userSession.getFile(fileName, { decrypt: true });
 
-    if (typeof response === 'string') {
-      return JSON.parse(response);
-    }
-
-    return response;
-  } catch (e) {
-    // TODO log error
-    return null;
+  if (typeof response === 'string') {
+    return JSON.parse(response);
   }
+
+  return response;
 };
 
 export const putFile = async ({
@@ -28,11 +23,7 @@ export const putFile = async ({
     [name: string]: any;
   };
 }) => {
-  try {
-    await userSession.putFile(fileName, JSON.stringify(file), {
-      encrypt: true,
-    });
-  } catch (e) {
-    console.error(e);
-  }
+  await userSession.putFile(fileName, JSON.stringify(file), {
+    encrypt: true,
+  });
 };
